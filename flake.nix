27ea4a8b71d6f5b96df5d41bee0205cb974f70da
Fedor Ivanov <ivnfedor@gmail.com>
2024-10-07 14:24:34 +0300

    Fix formatting

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     nix-homebrew = {
       url = "github:zhaofengli-wip/nix-homebrew";
     };
@@ -26,44 +26,64 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nixpkgs-stable, home-manager, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask }:
-  let
-    system = "aarch64-darwin";
-    username = "fedorivn";
-    hostname = "${username}s-macbook-pro";
-    pkgs-stable = import nixpkgs-stable {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Fedors-MacBook-Pro
-    darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
-      inherit system;
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nixpkgs-stable,
+      home-manager,
+      nix-homebrew,
+      homebrew-bundle,
+      homebrew-core,
+      homebrew-cask,
+    }:
+    let
+      system = "aarch64-darwin";
+      username = "fedorivn";
+      hostname = "${username}s-macbook-pro";
+      pkgs-stable = import nixpkgs-stable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Fedors-MacBook-Pro
+      darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
+        inherit system;
 
-      specialArgs = { inherit inputs self system username pkgs-stable hostname; };
-      modules = [ 
-        ./system/default.nix
-        home-manager.darwinModules.home-manager
-        ./home/default.nix
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            user = username;
-            enable = true;
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-              "homebrew/homebrew-bundle" = homebrew-bundle;
+        specialArgs = {
+          inherit
+            inputs
+            self
+            system
+            username
+            pkgs-stable
+            hostname
+            ;
+        };
+        modules = [
+          ./system/default.nix
+          home-manager.darwinModules.home-manager
+          ./home/default.nix
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              user = username;
+              enable = true;
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = homebrew-bundle;
+              };
+              mutableTaps = false;
             };
-            mutableTaps = false;
-          };
-        }
-      ];
-    };
+          }
+        ];
+      };
 
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations.${hostname}.pkgs;
-  };
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations.${hostname}.pkgs;
+    };
 }
--- system/default.nix
+++ system/default.nix
@@ -27,82 +27,82 @@
   # system.defaults.dock.autohide-delay = 0.24;
   # system.defaults.dock.autohide-time-modifier = 1.0;
 
-      # TODO: what is that?
-    # services.lorri.enable
-    # services.jankyborders.enable
-    # services.dnsmasq.enable
-    # security.sandbox.profiles
-    # services.mopidy.enable
-    # services.netbird.enable
-    #  services.nextdns.enable
-    #  services.sketchybar.enable
-    #  services.skhd.enable 
-    #  services.spacebar.enable 
-    #  services.spotifyd.enable 
-    #  services.synapse-bt.enable 
-    #  services.synergy.package 
-    #  services.telegraf.enable 
-    #  services.yabai.enable 
+  # TODO: what is that?
+  # services.lorri.enable
+  # services.jankyborders.enable
+  # services.dnsmasq.enable
+  # security.sandbox.profiles
+  # services.mopidy.enable
+  # services.netbird.enable
+  #  services.nextdns.enable
+  #  services.sketchybar.enable
+  #  services.skhd.enable 
+  #  services.spacebar.enable 
+  #  services.spotifyd.enable 
+  #  services.synapse-bt.enable 
+  #  services.synergy.package 
+  #  services.telegraf.enable 
+  #  services.yabai.enable 
 
-    #  system.defaults.".GlobalPreferences"."com.apple.sound.beep.sound" 
-    #  system.defaults.ActivityMonitor.SortColumn
+  #  system.defaults.".GlobalPreferences"."com.apple.sound.beep.sound" 
+  #  system.defaults.ActivityMonitor.SortColumn
 
-    # !!! 
-    #  system.defaults.CustomSystemPreferences 
-    #  system.defaults.CustomUserPreferences 
-    #  system.defaults.NSGlobalDomain.
+  # !!! 
+  #  system.defaults.CustomSystemPreferences 
+  #  system.defaults.CustomUserPreferences 
+  #  system.defaults.NSGlobalDomain.
 
-    system.defaults = {
-      finder.CreateDesktop = false;
-      NSGlobalDomain = {
-        AppleInterfaceStyle = "Dark";
-        AppleScrollerPagingBehavior = true;
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true;
-        AppleShowScrollBars = "WhenScrolling";
-        # TODO: try this setting
-        # AppleSpacesSwitchOnActivate = false;
-        # InitialKeyRepeat = 1;
-        # KeyRepeat = 1;
+  system.defaults = {
+    finder.CreateDesktop = false;
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      AppleScrollerPagingBehavior = true;
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      AppleShowScrollBars = "WhenScrolling";
+      # TODO: try this setting
+      # AppleSpacesSwitchOnActivate = false;
+      # InitialKeyRepeat = 1;
+      # KeyRepeat = 1;
 
-        # NSAutomaticCapitalizationEnabled = true;
-        # NSAutomaticDashSubstitutionEnabled = true;
-        # NSAutomaticInlinePredictionEnabled = true;
-        # NSAutomaticPeriodSubstitutionEnabled = true;
-        # NSAutomaticQuoteSubstitutionEnabled = true;
-        # NSAutomaticSpellingCorrectionEnabled = true;
+      # NSAutomaticCapitalizationEnabled = true;
+      # NSAutomaticDashSubstitutionEnabled = true;
+      # NSAutomaticInlinePredictionEnabled = true;
+      # NSAutomaticPeriodSubstitutionEnabled = true;
+      # NSAutomaticQuoteSubstitutionEnabled = true;
+      # NSAutomaticSpellingCorrectionEnabled = true;
 
-        NSDocumentSaveNewDocumentsToCloud = false;
-        # Whether to enable moving window by holding anywhere on it like on Linux. The default is false.
-        # NSWindowShouldDragOnGesture = true;
-        "com.apple.keyboard.fnState" = true;
+      NSDocumentSaveNewDocumentsToCloud = false;
+      # Whether to enable moving window by holding anywhere on it like on Linux. The default is false.
+      # NSWindowShouldDragOnGesture = true;
+      "com.apple.keyboard.fnState" = true;
 
-        # system.defaults.NSGlobalDomain."com.apple.sound.beep.volume"
+      # system.defaults.NSGlobalDomain."com.apple.sound.beep.volume"
 
-        # TODO: what is that? 
-        #  system.defaults.WindowManager.AppWindowGroupingBehavior 
+      # TODO: what is that? 
+      #  system.defaults.WindowManager.AppWindowGroupingBehavior 
 
-        # system.defaults.dock.autohide-delay
-        # system.defaults.dock.autohide 
-        # system.defaults.dock.autohide-time-modifier
-        # system.defaults.dock.expose-animation-duration 
-        # system.defaults.dock.expose-group-by-app 
-        # system.defaults.dock.mineffect 
-        # system.defaults.dock.minimize-to-application = true;
-        # system.defaults.dock.persistent-apps 
-        # system.defaults.dock.show-recents 
-        # system.defaults.dock.tilesize = 64
-        # system.defaults.dock.wvous-br-corner = 1;
-        # system.defaults.finder.ShowPathbar = true;
-        # system.defaults.finder._FXShowPosixPathInTitle = true;
-        # system.defaults.screensaver.askForPassword = true;
-        # system.defaults.spaces.spans-displays = true;
-        # system.keyboard.enableKeyMapping 
-      };
-
-      #  time.timeZone 
-      #  users.users 
+      # system.defaults.dock.autohide-delay
+      # system.defaults.dock.autohide 
+      # system.defaults.dock.autohide-time-modifier
+      # system.defaults.dock.expose-animation-duration 
+      # system.defaults.dock.expose-group-by-app 
+      # system.defaults.dock.mineffect 
+      # system.defaults.dock.minimize-to-application = true;
+      # system.defaults.dock.persistent-apps 
+      # system.defaults.dock.show-recents 
+      # system.defaults.dock.tilesize = 64
+      # system.defaults.dock.wvous-br-corner = 1;
+      # system.defaults.finder.ShowPathbar = true;
+      # system.defaults.finder._FXShowPosixPathInTitle = true;
+      # system.defaults.screensaver.askForPassword = true;
+      # system.defaults.spaces.spans-displays = true;
+      # system.keyboard.enableKeyMapping 
     };
+
+    #  time.timeZone 
+    #  users.users 
+  };
 
   services.nix-daemon.enable = true;
   nix = {
