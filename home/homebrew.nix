5f42efc2cc03e978fb1cc68d2f5ac66f98f0417f
Fedor Ivanov <ivnfedor@gmail.com>
2024-10-27 14:48:21 +0300

    Refactor homebrew to greedily update casks

--- home/homebrew.nix
+++ home/homebrew.nix
@@ -4,22 +4,28 @@
     # Homebrew is *installed* via the flake input nix-homebrew
     enable = true;
 
-    casks = [
+    casks = map (name: {
+      name = name;
+      greedy = true;
+    }) [
       "firefox"
       "vlc"
       "telegram"
       "altserver"
       "beekeeper-studio"
+      "chromium"
+    ] ++ [
       # Touch ID unlock support on macOS isn't working for nixpkgs build of
       # KeePassXC, so the Homebrew version is used instead.
       # TODO: Track this issue - https://github.com/NixOS/nixpkgs/issues/241103
       "keepassxc"
-      "chromium"
     ];
 
-    global.autoUpdate = false;
+    caskArgs.no_quarantine = true;
+
     onActivation = {
       cleanup = "zap";
+      autoUpdate = true;
       upgrade = true;
     };
 
