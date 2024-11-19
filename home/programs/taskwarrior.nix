7e15871ffc5d1369cf78ed1e89ea3ee641f5499b
Fedor Ivanov <ivnfedor@gmail.com>
2024-11-19 10:47:49 +0300

    Add user defined attributes to Taskwarrior

--- home/programs/taskwarrior.nix
+++ home/programs/taskwarrior.nix
@@ -3,5 +3,9 @@
   programs.taskwarrior = {
     enable = true;
     package = pkgs.taskwarrior3;
+    config = {
+      uda.syncallduration.type="duration";
+      uda.syncallduration.label="Duration";
+    };
   };
 }
