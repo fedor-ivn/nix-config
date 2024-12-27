8c1e268a8244479666502050ad3261bb5516a0b6
Fedor Ivanov <ivnfedor@gmail.com>
2024-12-27 19:18:39 +0300

    Configure alacritty

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -1,6 +1,40 @@
 {
+  pkgs,
+  ...
+}:
+{
   programs.alacritty = {
     enable = true;
+    settings = {
+      colors = {
+        normal.black = "#565C63";
+        primary.background = "#252525";
+      };
+      cursor = {
+        blink_interval = 500;
+        blink_timeout = 0;
+        thickness = 0.25;
+        style = {
+          blinking = "Always";
+          shape = "Beam";
+        };
+      };
+      window = {
+        dynamic_padding = true;
+        opacity = 0.95;
+        blur = true;
+        dimensions = {
+          columns = 140;
+          lines = 42;
+        };
+        decorations = "Full";
+        padding = {
+          x = 8;
+          y = 8;
+        };
+        option_as_alt = "Both";
+      };
+    };
     # settings = {
     #   bell = {
     #     animation = "Ease";
