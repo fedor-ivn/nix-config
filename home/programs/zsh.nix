b9881f76156ae855c21d796834be5f751953ad4f
Fedor Ivanov <ivnfedor@gmail.com>
2024-12-28 12:49:57 +0300

    Add keybindings for beginning and end of line in zsh

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -16,6 +16,8 @@
     initExtra = ''
       bindkey "^[[1;3C" forward-word
       bindkey "^[[1;3D" backward-word
+      bindkey "D" beginning-of-line
+      bindkey "C" end-of-line
     '';
   };
 }
