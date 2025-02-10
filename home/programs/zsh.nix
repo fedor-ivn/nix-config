b9549e40d7189d3d154aca6950f129f68fdd58bd
Fedor Ivanov <ivnfedor@gmail.com>
2025-02-10 10:58:15 +0300

    Do not bind `D` and `C`

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -16,8 +16,6 @@
     initExtra = ''
       bindkey "^[[1;3C" forward-word
       bindkey "^[[1;3D" backward-word
-      bindkey "D" beginning-of-line
-      bindkey "C" end-of-line
     '';
   };
 }
