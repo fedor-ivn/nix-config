c00c9551887c05a0b595d695ef614bb1d0bde852
Fedor Ivanov <ivnfedor@gmail.com>
2025-04-06 14:38:47 +0300

    Configure sync server for Taskwarrior

--- home/programs/taskwarrior.nix
+++ home/programs/taskwarrior.nix
@@ -1,7 +1,23 @@
 { pkgs, ... }:
+let
+  taskSyncScript = pkgs.writeShellScriptBin "task-sync" ''
+    #!/usr/bin/env bash
+    task sync > /dev/null
+  '';
+  secrets = import ../../secrets/taskwarrior-sync.nix;
+in
 {
   programs.taskwarrior = {
     enable = true;
     package = pkgs.taskwarrior3;
+    config = {
+      sync.server.url = secrets.syncUrl;
+      sync.server.client_id = secrets.clientId;
+      sync.encryption_secret = secrets.encryptionSecret;
+      recurrence = "on";
+      uda.taskwarrior-tui.shortcuts."1" = "${taskSyncScript}/bin/task-sync";
+    };
   };
+
+  home.packages = [ taskSyncScript ];
 }
