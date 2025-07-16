925e052e11d4c07b6bb45cc1b3ffbf6034666640
Fedor Ivanov <ivnfedor@gmail.com>
2025-07-16 11:41:01 +0300

    Swap shortcuts for Taskwarrior TUI
    change order of `task-open-urls` and `task-sync`

--- home/programs/taskwarrior.nix
+++ home/programs/taskwarrior.nix
@@ -11,8 +11,8 @@ in
       sync.server.client_id = secrets.clientId;
       sync.encryption_secret = secrets.encryptionSecret;
       recurrence = "on";
-      uda.taskwarrior-tui.shortcuts."1" = "task-sync";
-      uda.taskwarrior-tui.shortcuts."2" = "task-open-urls";
+      uda.taskwarrior-tui.shortcuts."1" = "task-open-urls";
+      uda.taskwarrior-tui.shortcuts."2" = "task-sync";
     };
   };
 }
