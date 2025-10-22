df7c1bfa9f886b36490b2992064ebd9be2c61287
Fedor Ivanov <ivnfedor@gmail.com>
2025-10-22 17:06:28 +0300

    Add `today` context for Taskwarrior
    - Add `today` context
    - Change TUI quick tage from `next` to `today`

--- home/programs/taskwarrior.nix
+++ home/programs/taskwarrior.nix
@@ -11,13 +11,19 @@ in
       sync.server.client_id = secrets.clientId;
       sync.encryption_secret = secrets.encryptionSecret;
       recurrence = "on";
-      uda.taskwarrior-tui.shortcuts."1" = "task-open-urls";
-      uda.taskwarrior-tui.shortcuts."2" = "task-sync";
+      uda.taskwarrior-tui = {
+        shortcuts = {
+          "1" = "task-open-urls";
+          "2" = "task-sync";
+        };
+        quick-tag.name = "today";
+      };
       context = {
         main = "project.not:shopping";
         personal = "project.not:shopping project.not:blockscout";
         blockscout = "project:blockscout";
         shop = "project:shopping";
+        today = "(scheduled:today or due.before:tomorrow or due:tomorrow or +today)";
       };
     };
   };
