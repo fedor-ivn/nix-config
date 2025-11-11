d452532d04cba8b535a36f96cf4e3f185ce97148
Fedor Ivanov <ivnfedor@gmail.com>
2025-11-11 15:48:14 +0300

    Update git to use `settings` instead of deprecated options

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,12 +1,16 @@
 {
   programs.git = {
     enable = true;
-    userEmail = "ivnfedor@gmail.com";
-    userName = "Fedor Ivanov";
-    extraConfig = {
+
+    settings = {
+      user = {
+        email = "ivnfedor@gmail.com";
+        name = "Fedor Ivanov";
+        signingkey = "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILycRbR32YtN1cD0SkJOwwO1cZgpaKVjJs42nTNh2RCD ivnfedor@gmail.com";
+      };
+
       commit.gpgsign = true;
       gpg.format = "ssh";
-      user.signingkey = "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILycRbR32YtN1cD0SkJOwwO1cZgpaKVjJs42nTNh2RCD ivnfedor@gmail.com";
 
       push = {
         followTags = true;
@@ -20,30 +24,30 @@
       core.untrackedCache = true;
 
       color.ui = "auto";
+
+      alias = {
+        sw = "switch";
+        # View abbreviated SHA, description, and history graph of the latest 20 commits.
+        l = "log --pretty=oneline -n 20 --graph --abbrev-commit";
+        c = "commit";
+        s = "status";
+        a = "add";
+        pl = "pull";
+        p = "push";
+        pf = "push --force";
+        # Show the diff between the latest commit and the current state.
+        d = ''!"git diff-index --quiet HEAD -- || clear; git --no-pager diff --patch-with-stat"'';
+        # List aliases.
+        aliases = "config --get-regexp alias";
+        # Amend the currently staged files to the latest commit.
+        amend = "commit --amend --reuse-message=HEAD";
+        branches = "branch --all";
+      };
     };
 
     ignores = [
       ".direnv"
       ".envrc"
     ];
-
-    aliases = {
-      sw = "switch";
-      # View abbreviated SHA, description, and history graph of the latest 20 commits.
-      l = "log --pretty=oneline -n 20 --graph --abbrev-commit";
-      c = "commit";
-      s = "status";
-      a = "add";
-      pl = "pull";
-      p = "push";
-      pf = "push --force";
-      # Show the diff between the latest commit and the current state.
-      d = ''!"git diff-index --quiet HEAD -- || clear; git --no-pager diff --patch-with-stat"'';
-      # List aliases.
-      aliases = "config --get-regexp alias";
-      # Amend the currently staged files to the latest commit.
-      amend = "commit --amend --reuse-message=HEAD";
-      branches = "branch --all";
-    };
   };
 }
