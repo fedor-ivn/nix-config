706e4e6de92559693307a483e0c63e8af2935f68
Fedor Ivanov <ivnfedor@gmail.com>
2026-04-24 13:53:24 +0300

    Add Raycast mic toggle script and SwiftBar volume plugin

--- modules/home/raycast.nix
+++ modules/home/raycast.nix
@@ -29,4 +29,38 @@ lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
       ${pkgs.macism}/bin/macism com.apple.keylayout.RussianWin
     '';
   };
+
+  home.file.".config/raycast/scripts/mic-toggle.sh" = {
+    executable = true;
+    text = ''
+      #!/bin/bash
+
+      # Required parameters:
+      # @raycast.schemaVersion 1
+      # @raycast.title Toggle Microphone
+      # @raycast.mode silent
+
+      current=$(osascript -e "input volume of (get volume settings)")
+      if [ "$current" -eq 0 ]; then
+          osascript -e "set volume input volume 30"
+      else
+          osascript -e "set volume input volume 0"
+      fi
+    '';
+  };
+
+  home.file.".config/swiftbar/plugins/volume.250ms.sh" = {
+    executable = true;
+    text = ''
+      #!/bin/bash
+
+      vol=$(osascript -e "input volume of (get volume settings)")
+
+      if [ "$vol" -eq 0 ]; then
+          echo ":mic.slash.fill: | sfcolor=systemRed symbolize=true"
+      else
+          echo ":mic.fill: | sfcolor=systemGreen symbolize=true"
+      fi
+    '';
+  };
 }
